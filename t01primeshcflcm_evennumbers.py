0a0a0bb76eff4461f67620c0cfc4c3f362db9c7f
wonjinn <53069807+wonjinn@users.noreply.github.com>
2019-10-24 14:53:03 +0800

    Update t01primeshcflcm_evennumbers.py

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -1,6 +1,7 @@
 # What are the even numbers from 1 to num?
 
-num = 123
+num = 13
 for i in range(1, num+1):
   if i % 2 == 0:
     print(i, end=' ')
+# Therefore, the even nu,bers between 1 to num is 2 4 6 8 10 12
